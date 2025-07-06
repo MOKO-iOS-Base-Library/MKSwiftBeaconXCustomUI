6e8b6be4bb907fce84dd5fd7b4140cd39921a883
lovexiaoxia <aadyx2007@163.com>
2025-07-06 14:36:59 +0800

    Fix

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "MKSwiftBeaconXCustomUI",
     platforms: [
-        .iOS(.v14),  // 最低支持iOS 14
+        .iOS(.v16),  // 最低支持iOS 16
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
